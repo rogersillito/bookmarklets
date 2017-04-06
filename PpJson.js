c87d06e42877c964b3c0a6fad033063c37ef0499
Roger Sillito <roger.sillito@kykloud.com>
2017-04-06 14:48:34 +0100

    add pretty print json bookmarklet

---
+++ PpJson.js
@@ -0,0 +1,5 @@
+javascript:(function() {
+  /* pretty print a JSON string to the browser window */
+  var json = window.prompt('Paste JSON string in here:');
+  document.write('<pre>'+JSON.stringify(JSON.parse(json), null, 2)+'</pre>')
+})()
